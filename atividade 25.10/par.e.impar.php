81db1ae5c9515c0c0a1c57907a1c6e8dd7108706
Davibarreto49 <146985042+Davibarreto49@users.noreply.github.com>
2023-10-25 14:47:46 -0300

    atividade  par e impar

---
+++ atividade 25.10/par.e.impar.php
@@ -0,0 +1,20 @@
+<?php
+#número par
+$num = 0;
+while ($num <= 500) {
+    echo $num . " ";
+    $num += 2;
+}
+
+echo("<br><br>");
+
+#número impar
+$num = 499; 
+
+while ($num >= 0) {
+    echo $num . " ";
+    $num -= 2; 
+}
+
+
+?>
